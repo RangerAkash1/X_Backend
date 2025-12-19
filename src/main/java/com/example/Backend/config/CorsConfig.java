5812ce8f1e24494b3dd79cb928d08072fb9b2525
RangerAkash1 <akash13032002@gmail.com>
2025-12-20 00:59:18 +0530

    cors and security levels

--- src/main/java/com/example/Backend/config/CorsConfig.java
+++ src/main/java/com/example/Backend/config/CorsConfig.java
@@ -17,4 +17,66 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowCredentials(false)
                 .maxAge(3600);
     }
+
+    @Override
+    public void addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void configurePathMatch(org.springframework.web.servlet.config.annotation.PathMatchConfigurer configurer) {
+        WebMvcConfigurer.super.configurePathMatch(configurer);
+    }
+
+    @Override
+    public void configureContentNegotiation(org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer configurer) {
+        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+    }
+
+    @Override
+    public void configureAsyncSupport(org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer configurer) {
+        WebMvcConfigurer.super.configureAsyncSupport(configurer);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer configurer) {
+        WebMvcConfigurer.super.configureDefaultServletHandling(configurer);
+    }
+
+    @Override
+    public void addInterceptors(org.springframework.web.servlet.config.annotation.InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addFormatters(org.springframework.format.FormatterRegistry registry) {
+        WebMvcConfigurer.super.addFormatters(registry);
+    }
+
+    @Override
+    public void addArgumentResolvers(java.util.List<org.springframework.web.method.support.HandlerMethodArgumentResolver> resolvers) {
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    }
+
+    @Override
+    public void addReturnValueHandlers(java.util.List<org.springframework.web.method.support.HandlerMethodReturnValueHandler> handlers) {
+        WebMvcConfigurer.super.addReturnValueHandlers handlers);
+    }
+
+    @Override
+    public void configureMessageConverters(java.util.List<org.springframework.http.converter.HttpMessageConverter<?>> converters) {
+        WebMvcConfigurer.super.configureMessageConverters(converters);
+    }
 }
